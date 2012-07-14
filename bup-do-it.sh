a134f37d55eee6ea7641ab321db154c9e99a3e15
Christian Werner <christian.werner@the-wogo.com>
2012-07-14 21:20:03 +0200

    Fixed. Index Directory to selected dir

--- bup-do-it.sh
+++ bup-do-it.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BACKUP_DRIVE=/media/backup
-BUP_DIR=${BACKUP_DRIVE}/bup
+BUP=bup
+BUP_DIR=${BACKUP_DRIVE}/${BUP}
 
 mountit() {
 	mount ${BACKUP_DRIVE} -o remount,rw
@@ -9,20 +10,22 @@ mountit() {
 unmountit() {
         mount ${BACKUP_DRIVE} -o remount,ro
 }
+getOwner() {
+	echo $(ls -l ${BACKUP_DRIVE} | grep ${BUP} | awk '{print $3}')
+}
 
 mountit
 echo "beginning backup: $(date)" >> ${BACKUP_DRIVE}/backupError.log
 
-
-
-
-bup index --exclude=~./gvfs/ --exclude=~/.cache/ ~ >> ${BACKUP_DRIVE}/backupError.log
+bup -d ${BUP_DIR} index --exclude=~./gvfs/ --exclude=~/.cache/ ~ >> ${BACKUP_DRIVE}/backupError.log
 
 echo ""
 echo "beginning Saving: $(date)" >> ${BACKUP_DRIVE}/backupError.log
 
-bup save -n $(hostname) ~ >> ${BACKUP_DRIVE}/backupError.log
-
+bup -d ${BUP_DIR} save -n $(hostname) ~ >> ${BACKUP_DRIVE}/backupError.log
+echo ""
+echo "setting Directory Owner back to owner"
+chown -R $(getOwner):$(getOwner) $BUP_DIR
 unmountit
 echo "backup Done"
 echo ""
